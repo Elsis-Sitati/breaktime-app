822153dc9fea0d27b252b18f6f1847a6ddce7e33
Elsis-Sitati <sitatielsis@gmail.com>
2016-08-21 21:47:30 +0300

    [break.py] Make programme wait and open youtube url

--- break-time-app/break.py
+++ break-time-app/break.py
@@ -1,7 +1,16 @@
 import webbrowser
 import time
 
-# suspend the programme for 10 
-time.sleep(5)
-# opening the given url
-webbrowser.open("https://www.youtube.com/watch?v=1Vvkq2ts5q8")
+total_breaks = 3
+break_count = 0
+
+# telling us when the programme began
+print ('This programe started on ' + time.ctime())
+# declaring for loop tto count three times
+while(break_count <= total_breaks):
+
+	# suspend the programme for 2 hours
+	time.sleep(2*60*60)
+	# opening the given url
+	webbrowser.open("https://www.youtube.com/watch?v=1Vvkq2ts5q8")
+	break_count = break_count + 1
